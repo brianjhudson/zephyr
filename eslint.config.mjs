b34fc9559d5b87a06555d1cdfd13ba071980a996
brianjhudson <brianjasonhudson@gmail.com>
2025-09-06 13:49:27 -0500

    Clean up config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,26 +3,20 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginCypress from "eslint-plugin-cypress";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:cypress/recommended"
-  ),
-  {
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    plugins: [pluginCypress],
     rules: {
       "cypress/no-assigning-return-values": "error",
       "cypress/no-unnecessary-waiting": "error",
       "cypress/assertion-before-screenshot": "error",
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
