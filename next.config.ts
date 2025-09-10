a1d0f8e4f35197bec9ecc2277bc65ba51cf423fc
brianjhudson <brianjasonhudson@gmail.com>
2025-09-09 20:10:25 -0500

    Move unsplash carousel to r2

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["images.unsplash.com", "zephyr-images.brianjhudson.com"],
   },
 };
 
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
